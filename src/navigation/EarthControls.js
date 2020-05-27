6a642ffe6fca760a3b15a1c6f49a2037aa072909
Rune Tvilum <rune@addin.dk>
2020-05-27 11:03:36 +0200

    added keyboard navigation to EarthControls.js changed circle to point in DXFExporter.js included exports in Potree.js

--- src/navigation/EarthControls.js
+++ src/navigation/EarthControls.js
@@ -16,9 +16,27 @@ export class EarthControls extends EventDispatcher {
 
 		this.rotationSpeed = 10;
 
+		this.keys = {
+			FORWARD: [38],
+			BACKWARD: [40],
+			LEFT: [37],
+			RIGHT: [39],
+			UP: [18, 38],
+			DOWN: [18, 40],
+			YAWR: [16, 39],
+			YAWL: [16, 37],
+			PITCHUP: [16, 38],
+			PITCHDOWN: [16, 40]
+		}
+
 		this.fadeFactor = 20;
 		this.wheelDelta = 0;
 		this.zoomDelta = new THREE.Vector3();
+		this.yawDelta = 0
+		this.pitchDelta = 0
+		this.radiusDelta = 0
+		this.translationDelta = new THREE.Vector3(0, 0, 0)
+		this.translationWorldDelta = new THREE.Vector3(0, 0, 0)
 		this.camStart = null;
 
 		this.tweens = [];
@@ -149,12 +167,67 @@ export class EarthControls extends EventDispatcher {
 			this.zoomToLocation(e.mouse);
 		};
 
+		let previousTouch = null
+		const touchStart = (e) => {
+			previousTouch = e
+		}
+
+		const touchEnd = (e) => {
+			previousTouch = e
+		}
+
+		const touchMove = (e) => {
+			if (e.touches.length === 2 && previousTouch.touches.length === 2) {
+				const prev = previousTouch
+				const curr = e
+
+				const prevDX = prev.touches[0].pageX - prev.touches[1].pageX
+				const prevDY = prev.touches[0].pageY - prev.touches[1].pageY
+				const prevDist = Math.sqrt(prevDX * prevDX + prevDY * prevDY)
+
+				const currDX = curr.touches[0].pageX - curr.touches[1].pageX
+				const currDY = curr.touches[0].pageY - curr.touches[1].pageY
+				const currDist = Math.sqrt(currDX * currDX + currDY * currDY)
+
+				const delta = currDist / prevDist
+				const resolvedRadius = this.scene.view.radius + this.radiusDelta
+				const newRadius = resolvedRadius / delta
+				this.radiusDelta = newRadius - resolvedRadius
+
+				this.stopTweens()
+			} else if (e.touches.length === 3 && previousTouch.touches.length === 3) {
+				const prev = previousTouch
+				const curr = e
+
+				const prevMeanX = (prev.touches[0].pageX + prev.touches[1].pageX + prev.touches[2].pageX) / 3
+				const prevMeanY = (prev.touches[0].pageY + prev.touches[1].pageY + prev.touches[2].pageY) / 3
+
+				const currMeanX = (curr.touches[0].pageX + curr.touches[1].pageX + curr.touches[2].pageX) / 3
+				const currMeanY = (curr.touches[0].pageY + curr.touches[1].pageY + curr.touches[2].pageY) / 3
+
+				const delta = {
+					x: (currMeanX - prevMeanX) / this.renderer.domElement.clientWidth,
+					y: (currMeanY - prevMeanY) / this.renderer.domElement.clientHeight
+				}
+
+				this.panDelta.x += delta.x
+				this.panDelta.y += delta.y
+
+				this.stopTweens()
+			}
+
+			previousTouch = e
+		}
+
 		this.addEventListener('drag', drag);
 		this.addEventListener('drop', drop);
 		this.addEventListener('mousewheel', scroll);
 		this.addEventListener('mousedown', onMouseDown);
 		this.addEventListener('mouseup', onMouseUp);
 		this.addEventListener('dblclick', dblclick);
+		this.addEventListener('touchstart', touchStart)
+		this.addEventListener('touchend', touchEnd)
+		this.addEventListener('touchmove', touchMove)
 	}
 
 	setScene (scene) {
@@ -229,12 +302,96 @@ export class EarthControls extends EventDispatcher {
 		}
 	}
 
+	stopTweens() {
+		this.tweens.forEach(e => e.stop())
+		this.tweens = []
+	}
+
 	update (delta) {
 		let view = this.scene.view;
 		let fade = Math.pow(0.5, this.fadeFactor * delta);
 		let progression = 1 - fade;
 		let camera = this.scene.getActiveCamera();
-		
+		{ // accelerate while input is given
+			const ih = this.viewer.inputHandler
+			const moveForward = this.keys.FORWARD.every(e => ih.pressedKeys[e]) && Object.keys(ih.pressedKeys).length === 1
+			const moveBackward = this.keys.BACKWARD.every(e => ih.pressedKeys[e]) && Object.keys(ih.pressedKeys).length === 1
+			const moveLeft = this.keys.LEFT.every(e => ih.pressedKeys[e]) && Object.keys(ih.pressedKeys).length === 1
+			const moveRight = this.keys.RIGHT.every(e => ih.pressedKeys[e]) && Object.keys(ih.pressedKeys).length === 1
+			const moveUp = this.keys.UP.every(e => ih.pressedKeys[e])
+			const moveDown = this.keys.DOWN.every(e => ih.pressedKeys[e])
+			const yawRight = this.keys.YAWR.every(e => ih.pressedKeys[e])
+			const yawLeft = this.keys.YAWL.every(e => ih.pressedKeys[e])
+			const pitchUp = this.keys.PITCHUP.every(e => ih.pressedKeys[e])
+			const pitchDown = this.keys.PITCHDOWN.every(e => ih.pressedKeys[e])
+
+			if (this.lockElevation) {
+				const dir = view.direction
+				dir.z = 0
+				dir.normalize()
+
+				if (moveForward && moveBackward) {
+					this.translationWorldDelta.set(0, 0, 0)
+				} else if (moveForward) {
+					this.translationWorldDelta.copy(dir.multiplyScalar(this.viewer.getMoveSpeed()))
+				} else if (moveBackward) {
+					this.translationWorldDelta.copy(dir.multiplyScalar(-this.viewer.getMoveSpeed()))
+				}
+			} else if (moveForward && moveBackward) {
+				this.translationDelta.y = 0
+			} else if (moveForward) {
+				this.translationDelta.y = this.viewer.getMoveSpeed()
+			} else if (moveBackward) {
+				this.translationDelta.y = -this.viewer.getMoveSpeed()
+			}
+
+			if (moveLeft && moveRight) {
+				this.translationDelta.x = 0
+			} else if (moveLeft) {
+				this.translationDelta.x = -this.viewer.getMoveSpeed()
+			} else if (moveRight) {
+				this.translationDelta.x = this.viewer.getMoveSpeed()
+			}
+
+			if (moveUp && moveDown) {
+				this.translationWorldDelta.z = 0
+			} else if (moveUp) {
+				this.translationWorldDelta.z = this.viewer.getMoveSpeed()
+			} else if (moveDown) {
+				this.translationWorldDelta.z = -this.viewer.getMoveSpeed()
+			}
+			if (yawRight) {
+				this.yawDelta += 0.2
+			} else if (yawLeft) {
+				this.yawDelta -= 0.2
+			}
+			if (pitchUp) {
+				this.pitchDelta += 0.2
+			} else if (pitchDown) {
+				this.pitchDelta -= 0.2
+			}
+		}
+		{ // apply rotation
+			let yaw = view.yaw
+			let pitch = view.pitch
+
+			yaw -= this.yawDelta * delta
+			pitch -= this.pitchDelta * delta
+
+			view.yaw = yaw
+			view.pitch = pitch
+		}
+		// apply translation
+		view.translate(
+			this.translationDelta.x * delta,
+			this.translationDelta.y * delta,
+			this.translationDelta.z * delta
+		)
+		view.translateWorld(
+			this.translationWorldDelta.x * delta,
+			this.translationWorldDelta.y * delta,
+			this.translationWorldDelta.z * delta
+		)
 		// compute zoom
 		if (this.wheelDelta !== 0) {
 			let I = Utils.getMousePointCloudIntersection(
@@ -283,6 +440,11 @@ export class EarthControls extends EventDispatcher {
 		{
 			this.zoomDelta.multiplyScalar(fade);
 			this.wheelDelta = 0;
+			const attenuation = Math.max(0, 1 - this.fadeFactor * delta)
+			this.yawDelta *= attenuation
+			this.pitchDelta *= attenuation
+			this.translationDelta.multiplyScalar(attenuation)
+			this.translationWorldDelta.multiplyScalar(attenuation)
 		}
 	}
 };
