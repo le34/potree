a469ceb71d786f96111bd1bf060bfb77e61ce837
Rune <rune@addin.dk>
2021-03-17 18:59:57 +0100

    WebGL2RenderingContext

--- src/PotreeRenderer.js
+++ src/PotreeRenderer.js
@@ -281,7 +281,7 @@ class Shader {
 			}
 
 			// uniform blocks
-			if(gl instanceof WebGL2RenderingContext){ 
+			if(typeof WebGL2RenderingContext !== 'undefined' && gl instanceof WebGL2RenderingContext){ 
 				let numBlocks = gl.getProgramParameter(program, gl.ACTIVE_UNIFORM_BLOCKS);
 
 				for (let i = 0; i < numBlocks; i++) {
